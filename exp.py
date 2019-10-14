98ee1ae967c1c5941e18013246fa4d80728d5941
HATANO Ayane <21716073@jwu.ac.jp>
2019-10-14 16:03:33 +0900

    Add

--- exp.py
+++ exp.py
@@ -12,5 +12,21 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
+class Add(object):
+    __slot__=['left', 'right']
+    def __init__(self, a, b):
+        self.left = a #aとbは式
+        self.right = b
+    def eval(self):
+        return self.left.eval() + self.right.eval()
 
+e = Add(Val(1), Val(2)) #1+2
+assert e.eval() == 3
+
+# 次のステップ 1+2+3 ==> 6
+e = Add(Add(Val(1),Val(2)),Val(3))
+assert e.eval() == 6
+
+
+print()
 print()
