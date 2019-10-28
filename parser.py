159d4616a77b305d1d3cafd697ef49ed37a0f622
HATANO Ayane <21716073@jwu.ac.jp>
2019-10-28 16:37:51 +0900

    output Add, Mul

--- parser.py
+++ parser.py
@@ -18,13 +18,19 @@ print(s, s1, s2) # + 記号で分割
 '''
 
 def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        return Val(int(s))
-    else:
+    if s.find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1), parse(s2))
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1), parse(s2))
+    return Val(int(s))
 
-e = parse("1+2+3")
-print(e)
+e = parse("1*2+3")
+print(e, e.eval())
+e = parse("1+2*3")
+print(e, e.eval())
